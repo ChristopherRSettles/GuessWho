4d21d0bb0c9b949a8275af6db57470921c7cb355
Christopher Settles <Christopher Settles>
2017-02-10 10:31:46 -0600

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/guesswho/GuessWhoActivity.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.guesswho;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GuessWhoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guess_who);
+    }
+}
---
+++ app/src/main/java/com/bignerdranch/android/guesswho/QuizQuestion.java
@@ -0,0 +1,11 @@
+package com.bignerdranch.android.guesswho;
+
+/**
+ * Created by Chris on 2/9/2017.
+ */
+
+public class QuizQuestion {
+
+
+
+}
