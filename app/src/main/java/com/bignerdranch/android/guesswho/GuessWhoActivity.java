a6cf3df8cd6a6051887666648ba35e5e20930012
ChristopherRSettles <ChristopherRSettles@gmail.com>
2017-02-16 13:39:47 -0600

    Testing Commit Message

--- app/src/main/java/com/bignerdranch/android/guesswho/GuessWhoActivity.java
+++ app/src/main/java/com/bignerdranch/android/guesswho/GuessWhoActivity.java
@@ -2,12 +2,63 @@ package com.bignerdranch.android.guesswho;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class GuessWhoActivity extends AppCompatActivity {
 
+    Button button1, button2, button3, button4, nextButton;
+    ImageView quizImage;
+    int mCurrentIndex;
+    QuizQuestion questionList[] = {
+            new QuizQuestion(R.drawable.jimenonamonanona, R.string.Wrong11,R.string.Wrong12,R.string.Wrong13 , R.string.Correct1 ,R.string.Correct1),
+            new QuizQuestion(R.drawable.badluckbrian, R.string.Wrong21,R.string.Wrong22,R.string.Wrong23 , R.string.Correct2 ,R.string.Correct2),
+            new QuizQuestion(R.drawable.boromir, R.string.Wrong31,R.string.Wrong32,R.string.Wrong33 , R.string.Correct3 ,R.string.Correct3),
+            new QuizQuestion(R.drawable.gabenewell, R.string.Wrong41,R.string.Wrong42,R.string.Wrong43 , R.string.Correct4 ,R.string.Correct4),
+            new QuizQuestion(R.drawable.kevenhart, R.string.Wrong51,R.string.Wrong52,R.string.Wrong53 , R.string.Correct5 ,R.string.Correct5),
+            new QuizQuestion(R.drawable.mrbean, R.string.Wrong61,R.string.Wrong62,R.string.Wrong63 , R.string.Correct6 ,R.string.Correct6)
+    };
+
+//Hello
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guess_who);
+        quizImage = (ImageView) findViewById(R.id.imageView1);
+        button1 = (Button) findViewById(R.id.btn1);
+        button2 = (Button) findViewById(R.id.btn2);
+        button3 = (Button) findViewById(R.id.btn3);
+        button4 = (Button) findViewById(R.id.btn4);
+        setResources();
+
+        nextButton = (Button) findViewById(R.id.nextButton);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0){
+                updateQuestion();
+            }
+        });
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                quizImage.setImageResource(R.drawable.mrbean);
+            }
+        });
+
     }
+
+    public void updateQuestion() {
+        mCurrentIndex = (mCurrentIndex + 1) % questionList.length;
+        setResources();
+    }
+    public void setResources() {
+        quizImage.setImageResource(questionList[mCurrentIndex].getImageResId());
+        button1.setText(questionList[mCurrentIndex].getButtResId1());
+        button2.setText(questionList[mCurrentIndex].getButtResId2());
+        button3.setText(questionList[mCurrentIndex].getButtResId3());
+        button4.setText(questionList[mCurrentIndex].getButtResId4());
+    }
+
 }
--- app/src/main/java/com/bignerdranch/android/guesswho/QuizQuestion.java
+++ app/src/main/java/com/bignerdranch/android/guesswho/QuizQuestion.java
@@ -5,7 +5,68 @@ package com.bignerdranch.android.guesswho;
  */
 
 public class QuizQuestion {
+//Image res ID
+    //Strings of four trial answer
+    //Correct answer gussed
 
+    private int mImageResId;
+    private int mButtResId1, mButtResId2, mButtResId3, mButtResId4;
+    private int mCorrectguessResId;
 
+    QuizQuestion(int _imageRedId, int _guess1, int _guess2, int _guess3, int _guess4, int _correctguess) {
+        mImageResId = _imageRedId;
+        mCorrectguessResId = _correctguess;
+        mButtResId1 = _guess1;
+        mButtResId2 = _guess2;
+        mButtResId3 = _guess3;
+        mButtResId4 = _guess4;
+    }
 
+    public int getImageResId() {
+        return mImageResId;
+    }
+
+    public void setImageResId(int imageResId) {
+        mImageResId = imageResId;
+    }
+
+    public int getButtResId1() {
+        return mButtResId1;
+    }
+
+    public void setButtResId1(int buttResId1) {
+        mButtResId1 = buttResId1;
+    }
+
+    public int getButtResId2() {
+        return mButtResId2;
+    }
+
+    public void setButtResId2(int buttResId2) {
+        mButtResId2 = buttResId2;
+    }
+
+    public int getButtResId3() {
+        return mButtResId3;
+    }
+
+    public void setButtResId3(int buttResId3) {
+        mButtResId3 = buttResId3;
+    }
+
+    public int getButtResId4() {
+        return mButtResId4;
+    }
+
+    public void setButtResId4(int buttResId4) {
+        mButtResId4 = buttResId4;
+    }
+
+    public int getCorrectguessResId() {
+        return mCorrectguessResId;
+    }
+
+    public void setCorrectguessResId(int correctguessResId) {
+        mCorrectguessResId = correctguessResId;
+    }
 }
